0fc5064c2bc6f27efa96cb6668b3fcfeed050d49
Sudhee-bsp <sudheendrapai.b@gmail.com>
2022-07-20 00:24:40 +0530

    added darkmode theme

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import {
-  MDBContainer,
-  MDBNavbar,
-  MDBNavbarBrand
-} from 'mdb-react-ui-kit';
-import Logo from '../assets/log5.svg'
+import React, { useState, useEffect } from "react";
+import { MDBContainer, MDBNavbar, MDBNavbarBrand } from "mdb-react-ui-kit";
+import Logo from "../assets/log5.svg";
+
+import DarkMode from "./theme/darkmode";
 
 export default function Navbar() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("clipDarkMode") === "true") {
+      setDarkMode(true);
+    } else {
+      setDarkMode(false);
+    }
+  }, []);
+
   return (
     <>
-      <MDBNavbar light bgColor='light'>
+      <MDBNavbar
+        {...(darkMode ? 'dark bgColor = "dark"' : 'light bgColor = "light"')}
+      >
         <MDBContainer>
-          <MDBNavbarBrand href='./'>
+          <MDBNavbarBrand href="./">
             <img
               src={Logo}
-            //   width="150"
-              height='40'
-              alt='clipnow_logo'
-              loading='lazy'
+              //   width="150"
+              height="40"
+              alt="clipnow_logo"
+              loading="lazy"
             />
           </MDBNavbarBrand>
+          <DarkMode />
         </MDBContainer>
       </MDBNavbar>
     </>
